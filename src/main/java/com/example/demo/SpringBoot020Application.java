883ee208f64b46d70486b93ef6e21f35e01438c9
GBTC406007UR <GBTC406007UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-03-16 17:07:50 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBoot020Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot020Application.class, args);
+	}
+}
